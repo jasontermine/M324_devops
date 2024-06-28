9e1e29b8c484f7cb1af9a0e71c24475beff1cd83
jasontermine <jason.termine@wiss-edu.ch>
2024-06-28 18:13:17 +0200

    UPDATED: added two unit tests to check if components render as intended

--- Sidequests/SQ2A/M324_PROJEKT_TODOLIST/frontend/src/App.test.js
+++ Sidequests/SQ2A/M324_PROJEKT_TODOLIST/frontend/src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders correctly', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText('ToDo Liste')).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: 'Absenden' })).toBeInTheDocument();
+});
+
+test('handles input changes', () => {
+  render(<App />);
+  const input = screen.getByRole('textbox');
+  fireEvent.change(input, { target: { value: 'New Task' } });
+  expect(input.value).toBe('New Task');
 });
