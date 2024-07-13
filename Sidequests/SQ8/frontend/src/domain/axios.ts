269198ef5e45a2149f85ba6e08bac58bed019f75
CKnuchel <christoph.knuchel@gmail.com>
2024-07-13 10:45:18 +0200

    FIXED: DNS Error

--- Sidequests/SQ8/frontend/src/domain/axios.ts
+++ Sidequests/SQ8/frontend/src/domain/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const base = axios.create({
-  baseURL: "http://backend:8080/v1",
+  baseURL: "http://localhost:8080/v1",
   headers: {
     "Content-Type": "application/json"
   },
